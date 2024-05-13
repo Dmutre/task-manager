e1d6b7875c20a7123ad5353333fa296caab7ecd2
Dmutre <104783173+Dmutre@users.noreply.github.com>
2024-05-13 20:57:00 +0200

    added simple testing for adjusting CI

---
+++ backend/src/app.spec.ts
@@ -0,0 +1,12 @@
+
+describe('Test test for check does it work' , () => {
+
+  const testFunc = () => {
+    return 'Hello test';
+  }
+
+  it('Should return "Hello test"', () => {
+    const hello = testFunc();
+    expect(hello).toEqual('Hello test')
+  })
+})
